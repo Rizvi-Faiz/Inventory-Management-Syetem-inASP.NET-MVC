0ebeb8497e3e97b591347c91f3b2574603ee07e2
Rizvi Faiz Syed <rizvifaiz1073@gmail.com>
2024-09-05 08:39:32 +0530

    Initial Commit

---
+++ BK_28122023_IMS/Application/InventoryManagementSystem/InventoryManagementSystem/Migrations/20231215100459_trigger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class trigger : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "by",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "when",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmpId",
+                table: "Employee",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "by",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "when",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmpId",
+                table: "Employee",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
